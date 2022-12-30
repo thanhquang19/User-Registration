57f740e287b100f0bc62e9338b617bad8a5bdb60
Hoang Le <hoangle3013@gmail.com>
2022-12-29 21:24:40 -0800

    add private route

--- useregistration/src/App.js
+++ useregistration/src/App.js
@@ -1,22 +1,46 @@
 
 import Login from "./Pages/Log-in/Login.jsx";
-import UserProfile from "./Pages/UserProfile/UserProfile.jsx";
+import {BrowserRouter, Route, Routes, Link, Outlet} from 'react-router-dom';
+
+import UserRegistration from "./Pages/UserRegistration/UserRegistration.jsx";
+import PrivateRoutes from "./Routes/PrivateRoutes.jsx";
 
 function App() {
   return (
 
-    <div className="App">
+    <BrowserRouter>
 
+    <Routes>
+      <Route path='/' element={<PrivateRoutes/>} />
+      <Route path='/Login' element={<Login/>}/>
+      <Route path='/registration' element={<UserRegistration/>}/>
+       
+ 
+    </Routes>
 
-      User Registration
-      <Login/>
+      {/* <div> */}
+      {/*   <Link to='/'>User UserProfile</Link> */}
+      {/*   <br/> */}
+      {/*   <Link to='/login'>Login</Link> */}
+      {/*   <br/> */}
+      {/*   <Link to='/regitration'>User Registration</Link> */}
+      {/*    */}
+      {/* </div> */}
+      {/*  */}
+      {/*  */}
 
-      <UserProfile/>
+   
+   
+   
 
-      
+    
+    
+    
+    
+  
+    
 
-    </div>
-
+    </BrowserRouter>
   
   );
 }
