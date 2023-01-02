cd95bdb379e97720d39aabd2cc312de336aa17e3
Thanh Quang Phan <thanhquang19@gmail.com>
2023-01-02 10:23:38 -0600

    add function to call confirmation is forgot pwd modal

--- useregistration/src/Pages/Log-in/ForgotPassword.jsx
+++ useregistration/src/Pages/Log-in/ForgotPassword.jsx
@@ -5,7 +5,12 @@ import Modal from 'react-modal'
 Modal.setAppElement('#root');
 export default function ForgotPassword(props) {
 
-   
+   const [confirmation, getConfirmation]  = useState();
+
+   const handleGetPwd = (e) => {
+    e.preventDefault();
+    getConfirmation('a new password has been sent to your registered email')
+   }
 
     return (
         <Modal isOpen={props.isOpen} 
@@ -15,8 +20,8 @@ export default function ForgotPassword(props) {
                     <h4>enter your registered email</h4>
                     <input type='email' ></input>
                     <br/>
-                    <button id='getPwd-btn'>get new password</button>
-                    <div>confirmation</div>
+                    <button id='getPwd-btn' onClick={handleGetPwd}>get new password</button>
+                    <div>{confirmation}</div>
                     
                  </form>
         </Modal>
