3453f5e17d7e7cf8bc5f8308d6c3a72d147f41f5
Thanh Quang Phan <thanhquang19@gmail.com>
2023-01-07 22:45:35 -0600

    add layout, styles for user profile

--- useregistration/src/Pages/UserProfile/UserProfile.jsx
+++ useregistration/src/Pages/UserProfile/UserProfile.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import {React, useRef, useState} from 'react'
+import './UserProfile.css'
 
 export default function UserProfile(props) {
+  const oldPass = useRef();
+  const newPass = useRef();
+  const confirmedPass = useRef();
+  const [isChangePassShowed, getIsChangePassShowed] = useState(true);
+  const [confirmMsg, getConfirmMsg] = useState();
 
   const handleSignOut = (e) => {
     e.preventDefault();
     props.signOut();
   }
+
+  const showChangePass = (e) => {
+    e.preventDefault();
+    getIsChangePassShowed(!isChangePassShowed);
+  }
   return (
-    <div>
-      <h2>User Profile</h2>
+    <form id='user-profile'>
       <h3>{props.currentUser.authentication.username}</h3>
       <h3>{props.currentUser.email}</h3>
-      <button className='btn' id='signout-btn' onClick={handleSignOut}>sign-out</button>
-      <button className='btn' id='changePass-btn'>change password</button>
-    </div> 
+      <button className='btn' id='signout-btn' onClick={handleSignOut}>sign out</button>
+      <br/>
+      <button className='btn' id='changePass-btn' onClick={showChangePass}>change password</button>
+      <fieldset id='change-password-set' hidden={isChangePassShowed}>
+        <label for='old-pass'>current password</label>
+        <input ref={oldPass} id='old-password'></input>
+        <label for='new-pass'>new password</label>
+        <input ref={newPass} id='new-password'></input>
+        <label for='confirmed-pass'>retype the new password</label>
+        <input ref={confirmedPass} id='confirmed-password'></input>
+        <button>click to change</button>
+      </fieldset>
+    </form> 
     // at least add sign-out button to this page
   )
 }
